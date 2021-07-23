5bb32d0874730c555d11cf6afb22d940ee6d3b82
codingspirits <vincci.cheng.9@gmail.com>
2021-07-23 23:40:59 +1000

    Completed Assignment

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+/*function fetchbooks(){
+  fetch ('https://anapioficeandfire.com/api/books')
+  .then (function (response) {
+      return response.json()
+  })
+  .then (function (data) {
+      console.log(data)
+      arrayOfCharacters = []
+      for (i=0; i<data.length; i++){
+          for (j of data[i].characters){
+              arrayOfCharacters.push(j) //Then push or append everything into the array //[] or {} is this the difference between the two?
+          } 
+      }
+      console.log(arrayOfCharacters[1030])})}
+  fetchbooks()*/
+      //console.log(data[4].name
+     /*  for (i=0; i< data.length; i++){
+          //console.log('No of pages' + data[i].numberOfPages)
+          totalPages = totalPages + data[i].numberOfPages}
+      console.log(totalPages)
+      }
+          )
+          console.log(totalPages)
+          return totalPages 
+  )}
+
+      //console.log(data.results[4].name)//The 5th book in the series
+
+/*         data.results.forEach(
+          function(char){
+              console.log(char.name)
+          }) 
   
-}
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+fetchbooks()
+  
+
+//The 1031st character in the series
+
+function submitData(){
+
+}
+ */
+
+function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then (function(response){
+    return response.json()})
+  .then (function(json){
+    return renderBooks(json)})}
+
+function renderBooks(books){
+  const main = document.querySelector('main')
+  books.forEach(function(book){
+    const h2 = document.createElement('h2')
+    h2.innerText = book.name
+    main.appendChild(h2)
+  })}
+
+document.addEventListener("DOMContentLoaded", function() {fetchBooks()})
